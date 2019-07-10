ac3d25e62797ed0c6ebd0a2f5040f880803d66d6
CristobalSalazar <chris.salza@gmail.com>
2019-07-09 22:45:34 -0400

    added quote component

---
+++ src/components/Quote.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+export default class Quote extends Component {
+
+  componentDidMount() {
+    this.getData();
+  }
+  state = {
+    data: {}
+  }
+  styleGreen = {
+    color: '#5cb85c'
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.profile !== this.props.profile) {
+      this.getData();
+    }
+  }
+  getData = () => {
+    this.props.iex.stock.quote(this.props.profile).then(data => {
+      this.setState({ data });
+    })
+  }
+  showData() {
+    const { changePercent, latestPrice, change } = this.state.data;
+    const pos = { color: '#5cb85c' }
+    const neg = { color: '#cc3333' }
+    let choice;
+    if (changePercent > 0) {
+      choice = pos;
+    } else if (changePercent < 0) { choice = neg }
+    if (this.state.data) {
+      return (
+        <div>
+          <h1>Quote</h1>
+          <br></br>
+          <p>Latest Price</p>
+          <h1><small>$ </small>{latestPrice}</h1>
+          <br />
+          <p>Change</p>
+          <span>
+            <h1 style={choice}><small style={choice}>$ </small>{change}</h1>
+            <h1 style={choice}><small style={choice}>% </small>{(changePercent * 100).toFixed(2)}</h1>
+          </span>
+          <br></br>
+        </div >
+      );
+    } else { }
+  }
+  render() {
+    return (
+      <div>
+        {this.showData()}
+      </div>
+    )
+  }
+}
